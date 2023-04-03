e776f6cf4c8e56b4b4786c66d439c21c3f196b1d
Tomasz Stawiarski <stawiar92@gmail.com>
2023-04-03 22:45:06 +0200

    Init project shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -1,2 +1,6 @@
 shopping_list = {"piekarnia": ["chleb", "bułki", "rogale"],
                  "warzywniak": ["brokuł", "ogórki", "szczypiorek"]}
+for shop, product in shopping_list.items():
+    print(f"Idę do {shop} i kupuję tam {product}")
+
+products = 0
