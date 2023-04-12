db30f7489e60935b863e15571d61beb1308bfa8e
Tomasz Stawiarski <stawiar92@gmail.com>
2023-04-12 23:04:11 +0200

    Add shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -4,3 +4,5 @@ for shop, product in shopping_list.items():
     print(f"Idę do {shop.title()} i kupuję tam {str(product).title()}")
 
 products = 0
+for values in shopping_list.values():
+    products += len(values)
